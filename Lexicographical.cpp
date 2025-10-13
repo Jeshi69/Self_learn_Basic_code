ee5ca32accc116687a0039d3aedf008ea9685baf
Miftaul Jannat <123179272+Jeshi69@users.noreply.github.com>
2025-10-13 15:22:43 +0600

    Add files via upload

---
+++ Lexicographical.cpp
@@ -0,0 +1,19 @@
+// string compare 
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,t;
+   cin>> s >> t;
+
+    if(s==t){
+        cout << "Same";          
+    }
+    else if(s<t){
+        cout << "A choto";       
+    }
+    else{
+        cout <<"B choto";       
+    }
+
+    return 0;
+}
