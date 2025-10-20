4bc666c46dde602659006d654d798310792a577a
Miftaul Jannat <123179272+Jeshi69@users.noreply.github.com>
2025-10-20 09:42:23 +0600

    Add files via upload

---
+++ string-size.cpp
@@ -0,0 +1,12 @@
+// string size ber kora
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << s << " ";// string print with space
+    cout << s.length();// size ber kora
+
+    return 0;
+}
